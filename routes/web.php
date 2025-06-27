6af3276dc1e6554ef541b5c2865b36100e978c05
andrijanikic23 <andrijanikic001@gmail.com>
2025-06-27 11:47:21 +0200

    * Added middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,52 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ShopController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [\App\Http\Controllers\HomepageController::class, 'index']);
+Route::get("/", [HomepageController::class, 'index']);
 
 Route::view("/about", "about");
 
 
-
-
-Route::get("/admin/all-contacts", [\App\Http\Controllers\ContactController::class, 'get_all_contacts']);
-
-
-
-
-Route::get("/admin/all-products", [\App\Http\Controllers\ShopController::class, 'get_all_products']);
-Route::get("/admin/delete-product/{product}", [\App\Http\Controllers\ShopController::class, "delete"]);
-
 Route::view("/contact", "contact");
-Route::post("/send-contact", [\App\Http\Controllers\ContactController::class, "sendContact"])
+Route::post("/send-contact", [ContactController::class, "sendContact"])
     ->name("send.contact");
 
-Route::view("/admin/add-product", "add_product");
-Route::post("/send-product", [\App\Http\Controllers\ShopController::class, "add_product"]);
 
 
 
 
-Route::get("/admin/delete-contact/{contact}", [\App\Http\Controllers\ContactController::class, "delete"]);
+Route::middleware(["auth", AdminCheckMiddleware::class])->prefix("admin")->group(function(){
 
-//HOMEWORK
-Route::get("/admin/product/edit/{product}", [\App\Http\Controllers\ShopController::class, "single_product"])
-    ->name("product.single");
+    Route::get("/all-products", [ShopController::class, 'get_all_products'])
+        ->middleware("auth")
+        ->name("allProducts");
+    Route::get("/delete-product/{product}", [ShopController::class, "delete"]);
+    Route::view("/add-product", "add_product");
+    Route::get("/delete-contact/{contact}", [ContactController::class, "delete"]);
 
-Route::post("/admin/product/save/{product}", [\App\Http\Controllers\ShopController::class, "edit"])
-    ->name("product.save");
 
-//HOMEWORK
-Route::get("/admin/contact/edit/{id}", [\App\Http\Controllers\ContactController::class, "single_contact"])
-    ->name("contact.single");
+    Route::get("/product/edit/{product}", [ShopController::class, "single_product"])
+        ->name("product.single");
 
-Route::post("/admin/contact/save/{id}", [\App\Http\Controllers\ContactController::class, "edit"])
-    ->name("contact.save");
+    Route::post("/product/save/{product}", [ShopController::class, "edit"])
+        ->name("product.save");
+
+    Route::get("/contact/edit/{id}", [ContactController::class, "single_contact"])
+        ->name("contact.single");
+
+    Route::post("/contact/save/{id}", [ContactController::class, "edit"])
+        ->name("contact.save");
+
+    Route::post("/send-product", [ShopController::class, "add_product"]);
+    Route::get("/all-contacts", [ContactController::class, 'get_all_contacts']);
+});
+
+
 
 
 require __DIR__.'/auth.php';
