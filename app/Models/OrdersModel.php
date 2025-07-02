22ec2f0de8fc30d2b3a7ee4883703a1cd59d52d9
andrijanikic23 <andrijanikic001@gmail.com>
2025-07-02 16:44:16 +0200

    * Order and order items tables

---
+++ app/Models/OrdersModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrdersModel extends Model
+{
+
+    protected $table = 'orders';
+    protected $fillable = ["user_id", "price", "status"];
+
+
+}
